0b80a48cebb0310f3f622b71767cb106d5b482fc
jaafar91 <m.jaafar.91@gmail.com>
2021-09-09 00:18:47 +0300

    remove column names

--- examples/example2/main/ddaa_template_parser.py
+++ examples/example2/main/ddaa_template_parser.py
@@ -8,7 +8,5 @@ def getApiTemplate(templateFileName,table,methods):
             line = line.replace("${ROUTE_NAME}",table["table"])
         if line.find("${ROUTE_METHODS}") > -1:
             line=line.replace("${ROUTE_METHODS}",methods)
-        if line.find("${COLUMNS_NAMES}") > -1:
-            line=line.replace("${COLUMNS_NAMES}",','.join(table["cols"]))
         destLines.append(line)
     return destLines
