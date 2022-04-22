f6771306425bb6a9f70717fcf43fc929a88757e2
jaafar91 <m.jaafar.91@gmail.com>
2022-04-22 15:08:07 +0300

    refactor

--- examples/example2/main/uda_template_parser.py
+++ examples/example2/main/uda_template_parser.py
@@ -68,8 +68,6 @@ def replaceJava(path,table):
         if line.find("${MODEL_COLUMNS}") > -1:
             modelLines=[]
             for col in table['cols']:
-                print(table)
-                print(col["dataType"])
                 if col["dataType"] == "bit":
                     col["dataType"] = "Boolean"
                 modelLines.append("private %s %s;"%(convertDatabaseDataTypeToJaveDateType(col["dataType"]),col["name"]))
